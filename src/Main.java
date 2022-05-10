9c5b310f13341aaaddf2bebacaa6fa8a1dc5d43f
viruslm <viruslm@mail.ru>
2022-05-10 10:02:41 +0300

    out for mergers

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,7 @@
 public class Main {
     public static void main(String[] args) {
         System.out.println("Вывод из мастер ветки");
+
+        System.out.println("Внесли изменения, чтоб слить две ветки");
     }
 }
