6b03e9a6697054cb9c3b5bc443ec8b253dbefda0
viruslm <viruslm@mail.ru>
2022-05-10 09:53:21 +0300

    new phone contact added

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,10 @@
 public class Main {
     public static void main(String[] args) {
         System.out.println("Вывод из мастер ветки");
+
+        String phone = "89256663300";
+        System.out.println("Звоните нам по телефону - " + phone);
+        
+
     }
 }
