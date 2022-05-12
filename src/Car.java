79c085b43b94c1c5a7f43eedd978778731b62b34
viruslm <viruslm@mail.ru>
2022-05-12 23:07:18 +0300

    headers changed in class-files

--- src/Car.java
+++ src/Car.java
@@ -1,3 +1,5 @@
 public class Car {
     int allCar = 10;
+
+    int most = 5;
 }
--- src/Contacts.java
+++ src/Contacts.java
@@ -1,3 +1,4 @@
 public class Contacts {
     String phone2 = "8 945 444 55 66";
+    String phone1 = "8 987 333 75 96";
 }
